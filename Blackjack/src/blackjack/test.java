775046f11db492c1cbfdd98f35ce0060a17e295f
enri10 <43589316+enri10@users.noreply.github.com>
2021-02-22 12:48:12 -0800

    Implemented 2 tests for Deck.java
    Tested the size of Deck to equal 52 and printed the contents of Deck object to verify the 52 unique Card objects

--- Blackjack/src/blackjack/test.java
+++ Blackjack/src/blackjack/test.java
@@ -1,7 +1,6 @@
 package blackjack;
 
 import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
 
 public class test {
@@ -10,5 +9,23 @@ public class test {
     public void testCard(){
         Card test = new Card(10, 'K', "Hearts");
         assertEquals("10 K Hearts", test.toString());
+    }
+
+    @Test
+    public void testDeckSize(){
+        Deck deck = new Deck();
+        assertEquals(52, deck.sizeOfDeck());
+
+    }
+
+    //Test to see if all cards are present in the deck
+    @Test
+    public void printDeck(){
+        Deck deck1 = new Deck();
+        Card card1;
+        for(int i = 0; i < deck1.sizeOfDeck(); i++){
+            card1 = deck1.getCardAtIndex(i);
+            System.out.println(card1.toString());
+        }
     }
 }
