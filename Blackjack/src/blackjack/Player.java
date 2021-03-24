a29eb5b3c489d932dc1581d362a6abd4e77f0583
enri10 <43589316+enri10@users.noreply.github.com>
2021-03-23 21:23:31 -0700

    Implemented functionality for an Ace
    When a bust occurs, if there is an ace present in hand it will be used as a 1 instead of 11

--- Blackjack/src/blackjack/Player.java
+++ Blackjack/src/blackjack/Player.java
@@ -16,9 +16,17 @@ public class Player extends Hand{
             return false;
         }
         System.out.println("Player's Hand: " + this.toString());
+
+        //If hand is greater than 21 and there is an Ace in hand, the ace will be used as a 1 instead of 11
         if(this.checkForBust()){
-            System.out.println("BUST!! : Total = " + this.getTotal());
-            return false;
+            if(this.getAceCount() > 0){
+                this.setTotal(this.getTotal() - 10);
+                this.setAceCount(this.getAceCount() -1);
+            }
+            else{
+                System.out.println("BUST!! : Total = " + this.getTotal());
+                return false;
+            }
         }
         System.out.println("Enter S for Stay or H for Hit");
         this.decision = in.next().charAt(0);
