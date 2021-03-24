699d9cbf415e2a8e91c122585e2e8bbe33e08437
enri10 <43589316+enri10@users.noreply.github.com>
2021-03-24 11:36:02 -0700

    Comments
    Commented working code

--- Blackjack/src/blackjack/Dealer.java
+++ Blackjack/src/blackjack/Dealer.java
@@ -3,12 +3,11 @@ package blackjack;
 public class Dealer extends Hand{
     protected Deck deck;
 
-
-    
     Dealer(Deck d){
         this.deck = d;
     }
 
+    //Dealer will draw cards until his total is 17 or greater
     public void dealerTurn(){
         if(this.getTotal() > 16 && this.getTotal() <= 21){
             return;
@@ -26,6 +25,7 @@ public class Dealer extends Hand{
         }
     }
 
+    //Print dealer hand with first card "face-up" and second card "face-down"
     public void printDealerHand(){
         String firstCard = "["+ this.getHand().get(0).getRank() + " " + this.getHand().get(0).getSuit() + "]";
         System.out.println("Dealer's Hand: " + firstCard + "[ ]");
