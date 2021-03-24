3d6ca95c28522fa74c687cddb1e20493383085f2
enri10 <43589316+enri10@users.noreply.github.com>
2021-03-23 20:28:12 -0700

    Implemented playerTurn() method
    Asks for user input to determine if player will stay or keep drawing cards

--- Blackjack/src/blackjack/Player.java
+++ Blackjack/src/blackjack/Player.java
@@ -1,19 +1,37 @@
 package blackjack;
 
+import java.util.Scanner;
+
 public class Player extends Hand{
     protected Deck deck;
+    protected char decision;
 
     Player(Deck d){
         this.deck = d;
     }
 
+    //Ask for user input to determine if the player will stay or keep drawing cards
+    public boolean playerTurn(Scanner in){
+        if(this.checkForBlackjack()){
+            return false;
+        }
+        System.out.println("Player's Hand: " + this.toString());
+        if(this.checkForBust()){
+            System.out.println("BUST!! : Total = " + this.getTotal());
+            return false;
+        }
+        System.out.println("Enter S for Stay or H for Hit");
+        this.decision = in.next().charAt(0);
 
+        if(decision == 'S'){
+            return false;
+        }
+        else if(decision == 'H'){
+            this.deal(deck);
+            return true;
+        }
 
-
-
-
-
-
-
+        return false;
+    }
 
 }
